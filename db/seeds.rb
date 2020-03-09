3f9abc4b27fa75612813fb97a718c18dddd896d6
Anais Ahmed <anaismahmed@uchicago.edu>
2020-03-08 21:37:33 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 aw = Word.create(name: "ˀaw", pos: "noun", definition: "deer")
+ana = Word.create(name: "ˀana", pos: "interjection", definition: "look!, look there!")
