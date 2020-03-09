9d69d7c2fe3b2b60f1272535e4038623ed35c5a5
Anais Ahmed <anaismahmed@uchicago.edu>
2020-03-08 21:36:21 -0500

    added index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+aw = Word.create(name: "ˀaw", pos: "noun", definition: "deer")
