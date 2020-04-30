2d2d7b5d51abd876c9dd3434af9dd94f97349bca
Anais Ahmed <anaismahmed@uchicago.edu>
2020-04-30 12:58:20 -0500

    cors for testing

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module TonkawaDictionary
     # the framework and any gems in your application.
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'hidden-hollows-93851.herokuapp.com'
+        origins 'hidden-hollows-93851.herokuapp.com', 'localhost:3000'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
