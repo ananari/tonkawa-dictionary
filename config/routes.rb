d972756caa99cfa9df6ec71159bafc6b45d56fd3
Anais Ahmed <anaismahmed@uchicago.edu>
2020-04-30 11:17:22 -0500

    get to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :words
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/search', to: "words#search"
+  post '/search', to: "words#search"
   root to: "words#home"
 end
