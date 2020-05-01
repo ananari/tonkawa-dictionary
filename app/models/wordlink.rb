0f578297533da3f0e36b00cf29809673957726c2
Anais Ahmed <anaismahmed@uchicago.edu>
2020-05-01 14:48:01 -0500

    fixed function

--- app/models/wordlink.rb
+++ app/models/wordlink.rb
@@ -1,7 +1,7 @@
 class Wordlink < ApplicationRecord
   belongs_to :word
 
-  def self.create_both(id1, id2, typeof)
-    Wordlink.create(word_id: id1, word_2: id2, typeof: typeof) && Wordlink.create(word_id: id2, word_2: id1, typeof: typeof)
+  def self.create_both(id1, id2, reltype)
+    Wordlink.create(word_id: id1, word_2: id2, reltype: reltype) && Wordlink.create(word_id: id2, word_2: id1, reltype: reltype)
   end
 end
